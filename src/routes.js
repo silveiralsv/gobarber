11fb3b5131ad6af498e703823bf8876867f7d3e9
Dois2 <silveira.lsv@gmail.com>
2019-10-22 21:33:48 -0300

    [Fase 2 - Módulo 2] Upload de imagem

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
