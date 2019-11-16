49aeee8e89c213ecc6ffc3e3db3ecaa512740231
Lucas Silveira <silveira.lsv@gmail.com>
2019-11-15 22:35:32 -0300

    [ERR] Não abre avatar do user.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,9 +13,10 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.get('/providers', ProviderController.index);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.post('/sessions', SessionController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
